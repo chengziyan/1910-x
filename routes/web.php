655147947919bedcfbe206eb0ccbe9320c64f262
wangyanrui <3376155385@qq.com>
2020-07-18 09:24:59 +0800

    登录注册个人中心

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/reg','User\IndexController@reg');//注册
+Route::post('/user/reg','User\IndexController@regDo');//执行注册
+Route::get('/user/login','User\IndexController@login');//登录
+Route::post('/user/login','User\IndexController@loginDo');//执行登录
+Route::get('/user/center','User\IndexController@center');//个人中心
